0dcea195de5cbed76d54fafeb7b29e789b374afe
vargalott <156085832+vargalott@users.noreply.github.com>
2021-05-10 15:29:22 +0300

    fix const logic

--- include/qtm-calc/data.hpp
+++ include/qtm-calc/data.hpp
@@ -21,7 +21,7 @@ private:
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static void calc_fs_if_outdated(qtm const &qtm);
+  static void calc_fs_if_outdated(qtm &qtm);
 
 public:
   qtm_data(void) = delete;
@@ -33,49 +33,49 @@ public:
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_avg_queue(qtm const &qtm);
+  static double calc_avg_queue(qtm &qtm);
   /**
    * Perform calculation of the ETE for a given instance
    * of the queuing system and get the result
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_ete(qtm const &qtm);
+  static double calc_ete(qtm &qtm);
   /**
    * Perform calculation of the average time in queue for a given instance
    * of the queuing system and get the result
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_avg_time_queue(qtm const &qtm);
+  static double calc_avg_time_queue(qtm &qtm);
   /**
    * Perform calculation of the percent of served requests for a given instance
    * of the queuing system and get the result
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_perc_served_req(qtm const &qtm);
+  static double calc_perc_served_req(qtm &qtm);
   /**
    * Perform calculation of the average count of served requests for a given
    * instance of the queuing system and get the result
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_avg_count_served_req(qtm const &qtm);
+  static double calc_avg_count_served_req(qtm &qtm);
   /**
    * Perform calculation of the average count of requests for a given instance
    * of the queuing system and get the result
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_avg_count_req(qtm const &qtm);
+  static double calc_avg_count_req(qtm &qtm);
   /**
    * Perform calculation of the average count of unserved requests for a given
    * instance of the queuing system and get the result
    *
    * @param[in] qtm Instance of qtm::qtm core class
    */
-  static double calc_avg_count_unserved_req(qtm const &qtm);
+  static double calc_avg_count_unserved_req(qtm &qtm);
 };
 
 }; // namespace qtm
